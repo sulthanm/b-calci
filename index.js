fe99b7186f7b9031b7584e93f51a491e2c4fdc3b
sulthanm <sulthanso73@gmail.com>
2021-08-06 09:53:22 +0530

    allowing heroku to run on  available port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT =  process.env.port || 8080;
+const PORT =  process.env.PORT || 8080;
 const path = require('path');
 app.use(express.static('./'));
 
